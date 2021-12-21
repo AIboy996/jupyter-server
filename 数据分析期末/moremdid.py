c15dcdbd0f6b1d6aa799beae52633398ad7693d6
AIboy996 <2207854887@qq.com>
2021-12-21 09:42:09 +0800

    1221

--- 数据分析期末/moremdid.py
+++ 数据分析期末/moremdid.py
@@ -1,5 +1,7 @@
 import requests
+from functools import cache
 
+@cache
 def moremdid(n):
     '''
     读取本地的mdidlist.txt文件中的mdid，接着探索更多可能的mdid
@@ -11,10 +13,13 @@ def moremdid(n):
     with open('mdidlist.txt', mode='r') as f:
         mdid = list(map(lambda x: eval(x[:-1]), f.readlines()))
         f.close()
+    l_valid = []
     start = mdid[-1] + 1
     for i in range(start, start + n):
         if requests.get(f'https://www.bilibili.com/bangumi/media/md{i}'):
-            mdid.append(i)
+            l_valid.append(i)
+    mdid += l_valid
+    print(mdid[-10:])
     # 覆盖写，更新源文件中的mdidlist
     with open('mdidlist.txt', mode='w') as f:
         f.writelines(map(lambda x: str(x) + '\n', mdid))
@@ -24,6 +29,6 @@ def moremdid(n):
 
 
 if __name__ == '__main__':
-    for t in range(50):
+    for t in range(2):
         moremdid(1000)
         print(f'{t} comleted!')
